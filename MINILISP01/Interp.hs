4a591d7d819adf72a68595d5365a5853cda8acdb
manu-msr <manu@ciencias.unam.mx>
2025-09-08 21:20:58 -0600

    Refactor MiniLisp grammar and interpreter; add lexer implementation

--- MINILISP01/Interp.hs
+++ MINILISP01/Interp.hs
@@ -2,13 +2,28 @@ module Interp where
 
 import Grammars
 
+-- Función que modela la semántica operacional estructural (->)
+smallStep :: ASA -> ASA
+smallStep (Num n) = (Num n)
+smallStep (Boolean b) = (Boolean b)
+smallStep (Add (Num i) (Num d)) = Num (i + d)
+smallStep (Add (Num i) d) = Add (Num i) (smallStep d)
+smallStep (Add i d) = Add (smallStep i) d
+smallStep (Sub (Num i) (Num d)) = Num (i - d)
+smallStep (Sub (Num i) d) = Sub (Num i) (smallStep d)
+smallStep (Sub i d) = Sub (smallStep i) d
+smallStep (Not (Boolean b)) = Boolean (not b)
+smallStep (Not e) = Not (smallStep e)
+
+-- Función que aprovecha la equivalencia entre la semántica estructural y natural
+-- => equiv ->*
+-- Es decir, aplica de forma iterativa la función smallStep hasta llegar a un ASA irreducible 
 interp :: ASA -> ASA
 interp (Num n) = (Num n)
 interp (Boolean b) = (Boolean b)
-interp (Add i d) = Num ((numN (interp i)) + (numN (interp d)))
-interp (Sub i d) = Num ((numN (interp i)) - (numN (interp d)))
-interp (Not e) = Boolean (not (boolN (interp e)))
+interp e = interp (smallStep e)
 
+-- Funciones auxiliares para extraer valores de ASA
 numN :: ASA -> Int
 numN (Num n) = n
 
--- MINILISP01/MiniLisp.hs
+++ MINILISP01/MiniLisp.hs
@@ -1,34 +1,27 @@
 module REPL where
 
+import Lex
 import Grammars
 import Interp
 
+-- Convierte un ASA en su representación textual
 saca :: ASA -> String
-saca (Num n) = show n
-saca (Boolean b)
-    | b == True = "#t"
-    | otherwise = "#f"
+saca (Num n)     = show n
+saca (Boolean b) = if b then "#t" else "#f"
 
--- Función encargada de llevar la ejecución del programa mediante los siguientes pasos:
--- 1. Impresión del propt.
--- 2. Lectura de una cadena.
--- 3. Si la cadana es igual a ":q", se cierra el intérprete.
--- 4. En caso contrario, realiza la generación de código ejecutable aplicando los análisis en
---    orden siguiente: léxico, sintáctico, semántico.
--- 5. Vuelve a ejecutar el ciclo.
-repl = 
-    do
-        putStr "> "
-        str <- getLine
-        if str == "(exit)" then 
-            putStrLn "Bye." 
-        else 
-            do
-                putStrLn $ saca (interp (parse (lexer str)))
-                repl
+-- Ciclo interactivo (REPL)
+repl :: IO ()
+repl = do
+  putStr "> "
+  str <- getLine
+  if str == "(exit)"
+    then putStrLn "Bye."
+    else do
+      putStrLn $ saca (interp (parse (lexer str)))
+      repl
 
--- Función principal. Da la bienvenida al usuario y ejecuta el REPL.
-run =
-    do
-        putStrLn "Mini-Lisp v1.0. Bienvenidx." 
-        repl
+-- Inicia el intérprete
+run :: IO ()
+run = do
+  putStrLn "Mini-Lisp v1.0. Bienvenidx."
+  repl
